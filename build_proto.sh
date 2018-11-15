815b4f59033373be1499ec4ad32804d65d259e6a
John Cornish <jcornish@pivotal.io>
2018-11-15 08:43:21 -0700

    build_proto.sh: include GRPC

--- build_proto.sh
+++ build_proto.sh
@@ -1,1 +1,6 @@
-protoc -I=flextime-api/v1/ --go_out=rpc/flextime_v1/ flextime-api/v1/*
+#!/usr/bin/env bash
+
+proto_dir=flextime-api/v1
+protoc $proto_dir/*.proto \
+    --go_out=plugins=grpc:rpc/flextime_v1 \
+    --proto_path=$proto_dir
