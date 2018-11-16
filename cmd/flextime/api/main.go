cc704b08ac14e756582b34f7c8166d1cca99d511
John Cornish <jcornish@pivotal.io>
2018-11-16 07:47:50 -0700

    cmd/api: clean up old code

--- cmd/flextime/api/main.go
+++ cmd/flextime/api/main.go
@@ -32,66 +32,11 @@ func Run() error {
 	server := grpc.NewServer()
 	v1.RegisterRESTServer(server, newTaskServer())
 	server.Serve(listen)
-	// println("****am listen")
 	return nil
 }
 
 func main() {
-	// tasks := []flextime.Task{
-	// 	{
-	// 		Name: "task-1",
-	// 	},
-	// }
-
-	// http.HandleFunc("/v1/tasks/", func(w http.ResponseWriter, r *http.Request) {
-	// 	b, _ := json.Marshal(tasks)
-	// 	w.Write(b)
-	// })
-
-	// println("listening on port 8080")
-	// http.ListenAndServe(":8080", nil)
 	if err := Run(); err != nil {
 		log.Fatal(err)
 	}
 }
-
-// import (
-// 	"flag"
-
-// 	"net"
-
-// 	"github.com/golang/glog"
-// 	"golang.org/x/net/context"
-// 	"google.golang.org/grpc"
-// )
-
-// type echoServer struct{}
-
-// func newEchoServer() pb.EchoServiceServer {
-// 	return new(echoServer)
-// }
-
-// func (s *echoServer) Echo(ctx context.Context, msg *pb.Message) (*pb.Message, error) {
-// 	glog.Info(msg)
-// 	return msg, nil
-// }
-
-// func Run() error {
-// 	listen, err := net.Listen("tcp", ":50051")
-// 	if err != nil {
-// 		return err
-// 	}
-// 	server := grpc.NewServer()
-// 	pb.RegisterEchoServiceServer(server, newEchoServer())
-// 	server.Serve(listen)
-// 	return nil
-// }
-
-// func main() {
-// 	flag.Parse()
-// 	defer glog.Flush()
-
-// 	if err := Run(); err != nil {
-// 		glog.Fatal(err)
-// 	}
-// }
--- cmd/flextime/api/main_test.go
+++ cmd/flextime/api/main_test.go
@@ -28,24 +28,6 @@ var _ = Describe("Main", func() {
 		session.Kill()
 	})
 
-	// It("serves tasks", func() {
-	// 	conn, err := net.Dial("tcp", ":50051")
-	// 	if err != nil {
-	// 		log.Fatal("could not connect to TCP server: ", err)
-	// 	}
-	// 	Expect(err).ToNot(HaveOccurred())
-	// 	_ = conn
-	// Eventually(func() int {
-	// 	resp, err := http.Get("http://127.0.0.1:8080/v1/tasks/")
-	// 	if err != nil {
-	// 		return -1
-	// 	}
-
-	// 	return resp.StatusCode
-	// }).Should(Equal(http.StatusOK))
-
-	// })
-
 	It("receives tasks and responds", func() {
 		taskResps := make(chan *v1.AddTaskResponse, 100)
 		var taskResp *v1.AddTaskResponse
@@ -69,31 +51,6 @@ var _ = Describe("Main", func() {
 
 		Expect(taskResps).To(Receive(&taskResp))
 	})
-
-	// It("returns an array of tasks", func() {
-	// 	tasks := []flextime.Task{
-	// 		{
-	// 			Name: "task-1",
-	// 		},
-	// 	}
-
-	// 	Eventually(func() []flextime.Task {
-	// 		// _, err := http.PostForm(
-	// 		// 	"http://127.0.0.1:8080/v1/tasks/",
-	// 		// 	url.Values{
-	// 		// 		"name": {
-	// 		// 			"task-1",
-	// 		// 		},
-	// 		// 		// "estimate": "15m",
-	// 		// 	},
-	// 		// )
-	// 		// if err != nil {
-	// 		// 	return []flextime.Task{}
-	// 		// }
-
-	// 		return getTasksFromJson("http://127.0.0.1:8080/v1/tasks/")
-	// 	}).Should(Equal(tasks))
-	// })
 })
 
 func getTasksFromJson(url string) []flextime.Task {
@@ -114,7 +71,6 @@ func startFlextime() *gexec.Session {
 	}
 
 	cmd := exec.Command(path)
-	// cmd.Env = envs
 	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 	if err != nil {
 		panic(err)
